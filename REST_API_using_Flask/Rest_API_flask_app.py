7cd3c9dec025247e71e470bb2f175af6f478253c
Yusra Celikel <yusragcelikel@gmail.com>
2022-11-03 22:38:20 +0300

    BMG3_homework3

---
+++ REST_API_using_Flask/Rest_API_flask_app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Hello(Resource):
+
+    def get(self):
+        return jsonify({'message': 'Hi There. This is BMG Homework!'})
+
+    def post(self):
+        data = request.get_json()
+        return jsonify({'data': data}), 201
+
+
+# bir sayının karesini hesaplamak için başka bir kaynak
+class Square(Resource):
+
+    def get(self, num):
+        return jsonify({'square': num ** 2})
+
+
+# karşılık gelen url'leriyle birlikte kaynakları ekleme
+api.add_resource(Hello, '/')
+api.add_resource(Square, '/square/<int:num>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
